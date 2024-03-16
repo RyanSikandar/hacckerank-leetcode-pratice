1e1f3a12de5b6333642993a27d96f492323aec20
Ryan Sikandar <119738219+RyanSikandar@users.noreply.github.com>
2024-03-16 16:03:47 +0500

    Create 3SUM.py

---
+++ 3SUM.py
@@ -0,0 +1,31 @@
+class Solution:
+    def threeSum(self, nums):
+        res = []
+        nums.sort()
+
+        res = []
+        nums.sort()
+
+        for i, a in enumerate(nums):
+            # Skip positive integers
+            if a > 0:
+                break
+            #checks if the first number we get is a duplicate.
+            if i > 0 and a == nums[i - 1]:
+                continue
+            l, r = i + 1, len(nums) - 1
+            while l < r:
+                threeSum = a + nums[l] + nums[r]
+                if threeSum > 0:
+                    r -= 1
+                elif threeSum < 0:
+                    l += 1
+                else:
+                    res.append([a, nums[l], nums[r]])
+                    l += 1
+                    r -= 1
+                    #checks if the lp is duplicate
+                    while nums[l] == nums[l - 1] and l < r:
+                        l += 1
+                        
+        return res
